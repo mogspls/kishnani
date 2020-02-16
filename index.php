51b27142b99a623ece3807c9d185cd7383f9b7d0
Jose Miguel Anover <josemiguelanover@Miggys-Macbook-Pro-2.local>
2020-02-17 05:16:21 +0800

    Finished Homepage

--- index.php
+++ index.php
@@ -8,11 +8,25 @@
 
     <section class="darkblue">
         <div class="container flex">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae reprehenderit mollitia tempore corporis cum, fugit voluptatum soluta labore accusantium ut eum voluptate hic delectus optio repudiandae harum velit natus tempora?
-            Possimus veniam magni fugiat beatae, consequuntur natus nemo, sed modi exercitationem rem odio quam? Eius saepe fugit iure blanditiis quidem? Id illo et perspiciatis impedit doloribus deleniti tempore, neque quas!
-            Qui quis animi, fuga illum eligendi dignissimos blanditiis repellat quo dolores consequuntur quidem veniam nostrum id laboriosam iusto, explicabo maxime voluptatum. Tempora ipsa quaerat consequuntur consectetur omnis dolorem quisquam officia.
-            Accusamus repellendus, vel ab aspernatur sed explicabo corporis obcaecati voluptates distinctio laudantium ratione asperiores? Laudantium voluptatem perferendis optio eius expedita et, sunt aliquam, consequuntur labore vero eaque sequi quasi laboriosam.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae reprehenderit mollitia tempore corporis cum, fugit voluptatum soluta labore accusantium ut eum voluptate hic delectus optio repudiandae harum velit natus tempora? Possimus veniam magni fugiat beatae, consequuntur natus nemo, sed modi exercitationem rem odio quam? Eius saepe fugit iure blanditiis quidem? Id illo et perspiciatis impedit doloribus deleniti tempore, neque quas! Qui quis animi, fuga illum eligendi dignissimos blanditiis repellat quo dolores consequuntur quidem veniam nostrum id laboriosam iusto, explicabo maxime voluptatum. Tempora ipsa quaerat consequuntur consectetur omnis dolorem quisquam officia. Accusamus repellendus, vel ab aspernatur sed explicabo corporis obcaecati voluptates distinctio laudantium ratione asperiores? Laudantium voluptatem perferendis optio eius expedita et, sunt aliquam, consequuntur labore vero eaque sequi quasi laboriosam.</p>
             <img src="assets/img/bluesquare.png" alt="" style="width: 100%; max-width: 320px; display: flex; align-self: center;">
+        </div>
+    </section>
+    <section class="blue">
+        <div class="container flex">
+            <img src="assets/img/bluesquare.png" alt="" style="width: 100%; max-width: 320px; display: flex; align-self: center;">
+            <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto magnam, ex ipsam placeat enim, ratione accusamus laborum fugit. Explicabo impedit sapiente ad facere reiciendis quia exercitationem, omnis, repudiandae laudantium reprehenderit.</span><span>Vel consequatur ut aliquam similique minima numquam veniam fuga optio quos totam est animi nihil natus voluptatem eum debitis, iure accusantium id facilis et architecto unde. Culpa, quas nihil quia.</span><span>Tempora quidem dolore accusamus! Blanditiis aperiam ullam, ratione excepturi libero. Consectetur sequi, quam, vero quod similique voluptates perspiciatis ut facere repellendus, aliquid praesentium distinctio maiores adipisci? Sequi perferendis commodi nesciunt.</span></p>
+        </div>
+    </section>
+    
+    <section class="darkblue">
+        <div class="container flex" style="text-align: center; justify-content: center;">
+            <a href="#" style="text-align: center; display: block; padding: 15px 20px; text-decoration: none; color: #fff; font-size: 18px; font-family: 'Open Sans', sans-serif; background: #09337C;">Watch the Video</a>
+        </div>
+        <div class="container">
+            <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur excepturi nulla voluptatum in maxime fugit, assumenda maiores rerum, quos nesciunt dicta velit molestiae laudantium saepe rem cum. Laborum, dolorum, beatae!</span>
+            <span>Quam amet facilis harum voluptatibus dolor repudiandae dignissimos eveniet laudantium culpa ex! Unde velit cum itaque nesciunt obcaecati, aut, fugit harum quis, ipsam necessitatibus rerum distinctio ut quidem dolorem maxime.</span>
+            <span>Vel, voluptatibus nostrum. Architecto quae minima, vel similique fugit. Distinctio tempora, dolore aut quibusdam veritatis vel ducimus excepturi cupiditate delectus, expedita natus, voluptatibus in minima at et consectetur voluptatem itaque!</span></p>
         </div>
     </section>
 </main>
